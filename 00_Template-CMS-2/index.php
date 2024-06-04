21a66fd5961a15fee837443c14afff4e24d2be29
vorakornvt <vorakorn.vt@gmail.com>
2024-06-04 11:43:56 +1000

    Initial commit

---
+++ 00_Template-CMS-2/index.php
@@ -0,0 +1,19 @@
+<!-- HEADER.PHP -->
+<?php 
+  require "templates/header.php"
+?>
+
+  <main class="container mt-3">
+    <div class="alert alert-success" role="alert">You are logged in!</div>
+    <div class="alert alert-warning" role="alert">You are not logged in</div>
+
+    <section class="container mt-3 p-4 bg-light">
+      Welcome to Template<b>CMS</b> home page
+    </section>
+  </main>
+  
+
+<!-- FOOTER.PHP -->
+<?php 
+  require "templates/footer.php"
+?>
