f64a347d438dce9d2ecd070dde00792cadec5ac2
vorakornvt <vorakorn.vt@gmail.com>
2024-06-05 23:31:12 +1000

    create post

--- includes/createpost.inc.php
+++ includes/createpost.inc.php
@@ -1,6 +1,5 @@
 <?php
   // 1. Start Session: 
-  // NOTE: We only want users to create posts if they're logged in
   session_start();
 
   // 2. Check if the user clicked the submit button from the createpost form + user is logged in
@@ -13,10 +12,9 @@
     $imageURL = $_POST['imageurl'];
     $comment = $_POST['comment'];
     $artistName = $_POST['artistname'];
-    $userId = $_SESSION['userId']; // Fetching the user ID from the session
+    $userId = $_SESSION['userId']; 
 
     // 5. VALIDATION: Check if any fields are empty
-    // NOTE: We could add more validation, but keeping it simple for now
     if (empty($title) || empty($imageURL) || empty($comment)) {
       // ERROR: Redirect with error message via GET
       header("Location: ../createpost.php?error=emptyfields");
@@ -24,32 +22,25 @@
     }
 
     // 6. Save Post to DB using Prepared Statements
-    // (i) Declare SQL statement with placeholders for values
-    $sql = "INSERT INTO artwork_reviews (title, image_url, description, artist_name, idUsers) VALUES (?, ?, ?, ?, ?)";
-
-    // (ii) Prepare SQL statement
+    $sql = "INSERT INTO artReview (title, image_url, description, artist_name, idUsers) VALUES (?, ?, ?, ?, ?)";
     $statement = $conn->prepare($sql);
-
-    // (iii) Bind parameters and execute the statement
     $statement->bind_param("ssssi", $title, $imageURL, $comment, $artistName, $userId);
     $statement->execute();
 
-    // (iv) Check for errors
+    // 7. Check for errors
     if($statement->error){
       // ERROR: Unknown server error on saving to DB
       header("Location: ../createpost.php?error=servererror");
       exit();
     }
 
-    // (v) Post is saved to "artwork_reviews" table - redirect with success message
+    // 8. Post is saved to "artReview" table - redirect with success message
     header("Location: ../posts.php?post=success"); 
     exit();
 
-  // 7. Restrict Access to Script Page
-  // NOTE: For example, to access this script, the user MUST be LOGGED IN & MUST CLICK SUBMIT 
+  // 9. Restrict Access to Script Page
   } else {
     header("Location: ../createpost.php?error=forbidden");
     exit();
   }
 ?>
-
--- posts.php
+++ posts.php
@@ -1,118 +1,58 @@
-<!-- HEADER.PHP -->
 <?php 
-  require "./templates/header.php"
+require "./templates/header.php";
 ?>
 
-  <main class="container p-4 bg-light mt-3">
-    <?php
-      // 1. QUERY DATABASE for ALL POSTS
-      // NOTE: No need for prepared statements - NOT posting data from users to DB.  Simply requesting data from DB to be displayed = NO SQL INJECTIONS POSSIBLE!
+<main class="container p-4 bg-light mt-3">
+  <?php
+  // 1. QUERY DATABASE for ALL POSTS
+  require './includes/connect.inc.php';
 
-      // (i) Connect to Database
-      require './includes/connect.inc.php';
+  // 2. Declare SQL command to retrieve all rows from the artReview table
+  $sql = "SELECT id, title, imageurl, comment, websiteurl, websitetitle FROM artReview";
 
-      // (ii) Declare SQL command to DB to retrieve ALL rows from posts table in DB
-      $sql = "SELECT id, title, imageurl ,comment, websiteurl, websitetitle FROM posts";
+  // 3. Execute the query and store the result in a variable
+  $result = $conn->query($sql);
 
-      // (iii) Call query & store result in variable
-      $result = $conn->query($sql);
-    ?>
+  // 4. Error handling
+  if (!$result) {
+    echo '<div class="alert alert-danger" role="alert">Error: ' . $conn->error . '</div>';
+  } else {
+    // Display posts if query is successful
+    if ($result->num_rows > 0) {
+      // Loop through each row and display the data
+      while ($row = $result->fetch_assoc()) {
+        echo '
+        <div class="card border-0 mt-3" id="' . $row['id'] . '">
+          <img src="' . $row['imageurl'] . '" class="card-img-top post-image" alt="' . $row['title'] . '">
+          <div class="card-body">
+            <h5 class="card-title">' . $row['title'] . '</h5>
+            <p class="card-text">' . $row['comment'] . '</p>
+            <a href="' . $row['websiteurl'] . '" class="btn btn-primary w-100" target="_blank">' . $row['websitetitle'] . '</a>';
 
-    <?php 
-      // ERROR: ON DELETION OF POST 
-      if(isset($_GET['error'])){
-        // (i) Internal server error 
-        if ($_GET['error'] == "sqlerror" || $_GET['error'] == "servererror") {
-          $errorMsg = "An internal server error has occurred - please try again later";
+        // Check if user is logged in and display edit/delete buttons
+        if (isset($_SESSION['userId'])) {
+          echo '
+          <div class="admin-btn">
+            <a href="editpost.php?id=' . $row['id'] . '" class="btn btn-secondary mt-2">Edit</a>
+            <a href="includes/deletepost.inc.php?id=' . $row['id'] . '" class="btn btn-danger mt-2">Delete</a>
+          </div>';
         }
 
-        // (ii) Dynamic Error Alert based on Variable Value 
-        echo '<div class="alert alert-danger" role="alert">' . $errorMsg . '</div>';
-      
-      // SUCCESS: POST CREATE
-      } else if(isset($_GET['post']) == "success"){
-        echo '<div class="alert alert-success" role="alert">
-          Post created!
-        </div>';  
-
-      // SUCCESS: POST EDIT 
-      } else if(isset($_GET['edit']) == "success"){
-        echo '<div class="alert alert-success" role="alert">
-          Post edited!
-        </div>'; 
-
-      // SUCCESS: POST DELETE
-      } else if (isset($_GET['delete']) == "success"){
-        echo '<div class="alert alert-success" role="alert">
-          Post successfully deleted!
-        </div>';    
+        echo '
+          </div>
+        </div>';
       }
-    ?>
+    } else {
+      // No results found
+      echo "0 results";
+    }
+  }
 
-    <?php
-      // 2. CHECK FOR POSTS RETURNED RESULT & DISPLAY ON SUCCESS
-      // (2.i) Success: Display Posts
-      if($result->num_rows > 0){
+  // Close database connection
+  $conn->close();
+  ?>
+</main>
 
-        // 3. LOOP DATA INTO OUR BOOTSTRAP CARD TEMPLATE
-        // (3.i) New variable with default state
-        $output = "";
-
-        // (3.ii) Take result -> convert to array & then insert into While Loop
-        // NOTE: For EACH row in the array, we will execute this loop, so long as there are new rows - i.e. will execute for as many rows / positions in the array there are
-        // NOTE: We make the output = our looping card HTML!
-        while($row = $result->fetch_assoc()) {
-
-          // (3.iv) Join output cards together with .=
-          // NOTE: We can test - as now we have multiple cards (just needs to be dynamic!)
-          $output .= 
-
-          // (3.v) Dynamic Data into Cards using Concatenation of Variables
-          // ORDER (from easiest): title, comment, id, image, alt (with title), websiteurl, websitetitle
-          '
-            <div class="card border-0 mt-3" id="' . $row['id'] . '">
-              <img src="' . $row['imageurl'] . '" class="card-img-top post-image" alt="' . $row['title'] . '">
-              <div class="card-body">
-                <h5 class="card-title">' . $row['title'] . '</h5>
-                <p class="card-text">' . $row['comment'] . '</p>
-                <a href="' . $row['websiteurl'] . '" class="btn btn-primary w-100" target="_blank">' . $row['websitetitle'] . '</a>';
-                
-                // (3.vi) Restrict Edit/Delete Button ONLY to logged in users (can see by default!)
-                // NOTE: We will use if statement to check userId set inside SESSION
-
-                // (3.vii) Setup Edit/ Delete Pages
-                // NOTE: Whilst pages don't function yet - we can set it up by passing in id of the post to the href url via query string
-                if(isset($_SESSION['userId'])){
-                  $output .= '
-                  <div class="admin-btn">
-                    <a href="editpost.php?id=' . $row['id'] . '" class="btn btn-secondary mt-2">Edit</a>
-                    <a href="includes/deletepost.inc.php?id=' . $row['id'] . '" class="btn btn-danger mt-2">Delete</a>
-                  </div>';
-                }
-
-            $output .= 
-            '
-              </div>
-            </div>
-            ';
-        }
-        // (3.iii) Echo out the result of the loop
-        echo $output;
-        // NOTE: PROBLEM - Every time loop runs, we essentially assign HTML to output.  This means, everytime we loop, we keep replacing output over and over with the latest card. 
-        // FIXED: We can tell php to JOIN each card onto each other above in (iv) using ".="
-
-
-      // (2.ii) Error: Template Error Message
-      // NOTE: You can probably render a better error message
-      } else {
-        echo "0 results";
-      }
-      // (2.iii) Close Connection
-      $conn->close();
-    ?>
-  </main>
-
-<!-- FOOTER.PHP -->
 <?php 
-  require "./templates/footer.php"
+require "./templates/footer.php";
 ?>
