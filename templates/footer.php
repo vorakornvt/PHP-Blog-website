ec9655a82836a924d3f0449c8bf02453d08d89ff
vorakornvt <vorakorn.vt@gmail.com>
2024-06-05 15:35:15 +1000

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,20 @@
-  <!-- Flaticon Attribution -->
-  <small class="text-center text-muted d-block my-3">Icon made by <a href="https://www.flaticon.com/authors/freepik">Freepik</a> from <a href="http://www.flaticon.com/" target="_blank" rel="noopener noreferrer">www.flaticon.com</a></small>
+
+
+
+<!-- <footer class="collapse navbar-collapse justify-content-center">
+  <ul class="nav">
+    <li class="nav-item"><a class="nav-link link-dark" href="#help">Help</a></li>
+    <li class="nav-item"><a class="nav-link link-dark" href="#status">Status</a></li>
+    <li class="nav-item"><a class="nav-link link-dark" href="#about">About</a></li>
+    <li class="nav-item"><a class="nav-link link-dark" href="#careers">Careers</a></li>
+    <li class="nav-item"><a class="nav-link link-dark" href="#press">Press</a></li>
+    <li class="nav-item"><a class="nav-link link-dark" href="#blog">Blog</a></li>
+    <li class="nav-item"><a class="nav-link link-dark" href="#privacy">Privacy</a></li>
+    <li class="nav-item"><a class="nav-link link-dark" href="#terms">Terms</a></li>
+    <li class="nav-item"><a class="nav-link link-dark" href="#text-to-speech">Text to Speech</a></li>
+    <li class="nav-item"><a class="nav-link link-dark" href="#teams">Teams</a></li>
+  </ul>
+</footer> -->
   <!-- Bootstrap Script Bundle -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </body>
