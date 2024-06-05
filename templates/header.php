05fe1474263db93af8b286dbc46a86ed82aa6255
vorakornvt <vorakorn.vt@gmail.com>
2024-06-05 13:42:37 +1000

    add new logo

--- templates/header.php
+++ templates/header.php
@@ -23,7 +23,7 @@
   <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
-        <img src="./img/Asset4.png" alt="ArtifyLOGO" width="130">
+        <img src="./img/Asset5.png" alt="ArtifyLOGO" width="130">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
