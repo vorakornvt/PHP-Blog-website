fe9626fb77dedaad3435ce946957850b60d1c8b8
vorakornvt <vorakorn.vt@gmail.com>
2024-06-07 17:47:43 +1000

    v

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
+session_unset(); 
+session_destroy(); 
 header("Location: ../index.php?logout=success"); 
 exit();
 ?>
